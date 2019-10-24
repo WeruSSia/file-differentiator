fb053b7344e8a68b84290240218fb3f9989dc7f2
WeruSSia <werussia16@gmail.com>
2019-10-24 11:48:43 +0200

    FileDifferentiatot convertByteArrayToHexString() method implementation, after checking if fileContentAsByteArray is not null

--- src/file_differentiator/FileDifferentiator.java
+++ src/file_differentiator/FileDifferentiator.java
@@ -12,6 +12,10 @@ public class FileDifferentiator {
 
     public void differentiate(String filename) {
         byte[] fileContentAsByteArray = readFile(filename);
+        String fileContent;
+        if (fileContentAsByteArray != null) {
+            fileContent = convertByteArrayToHexString(fileContentAsByteArray);
+        }
     }
 
     private byte[] readFile(String filename) {
@@ -21,5 +25,13 @@ public class FileDifferentiator {
             System.out.println(e.getMessage());
         }
         return null;
+    }
+
+    private String convertByteArrayToHexString(byte[] data) {
+        StringBuilder stringHex = new StringBuilder();
+        for (byte b : data) {
+            stringHex.append(String.format("%02X", b));
+        }
+        return stringHex.toString();
     }
 }
