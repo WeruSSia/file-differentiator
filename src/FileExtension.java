c78355bf9520cc0b45fc37970cb1d05ca2cfe3d9
WeruSSia <werussia16@gmail.com>
2019-10-24 11:12:50 +0200

    FileExtension changed from interface to abstract class, getMagicNumbers() changed to containsMagicNumber(String magicNumber)

--- src/FileExtension.java
+++ src/FileExtension.java
@@ -1,6 +1,19 @@
 import java.util.Set;
 
-public interface FileExtension {
-    String getName();
-    Set<String> getMagicNumbers();
+public abstract class FileExtension {
+    private final String name;
+    private final Set<String> magicNumbers;
+
+    protected FileExtension(String name, Set<String> magicNumbers) {
+        this.name = name;
+        this.magicNumbers = magicNumbers;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean containsMagicNumber(String magicNumber){
+        return magicNumbers.contains(magicNumber);
+    }
 }
