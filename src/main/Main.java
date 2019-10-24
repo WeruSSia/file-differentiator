446d2138384752a4b21513278b15d9e89dc0075f
WeruSSia <werussia16@gmail.com>
2019-10-24 12:37:12 +0200

    Main class implementation - Scanner for user input

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 
-public class Main {
-    public static void main(String[] args) {
+import file_differentiator.FileDifferentiator;
+import file_differentiator.UnhandledExtensionException;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, UnhandledExtensionException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input file name: ");
+        String filename = scanner.nextLine();
+        new FileDifferentiator().differentiate(filename);
     }
 }
