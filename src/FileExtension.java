d4b831462895374e48b9aa78a36a42311ac28c25
WeruSSia <werussia16@gmail.com>
2019-10-24 11:17:19 +0200

    FileExtension became a regular class, not abstract

--- src/FileExtension.java
+++ src/FileExtension.java
@@ -1,10 +1,10 @@
 import java.util.Set;
 
-public abstract class FileExtension {
+public class FileExtension {
     private final String name;
     private final Set<String> magicNumbers;
 
-    protected FileExtension(String name, Set<String> magicNumbers) {
+    FileExtension(String name, Set<String> magicNumbers) {
         this.name = name;
         this.magicNumbers = magicNumbers;
     }
