6444df4e27d27b413dc65095bdbb059edc547a1f
WeruSSia <werussia16@gmail.com>
2019-10-24 11:43:42 +0200

    FileDifferentiator readFile() method

--- src/file_differentiator/FileDifferentiator.java
+++ src/file_differentiator/FileDifferentiator.java
@@ -1,12 +1,25 @@
 package file_differentiator;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileDifferentiator {
 
-    public FileDifferentiator(){
+    public FileDifferentiator() {
 
     }
 
-    public void differentiate(String filename){
+    public void differentiate(String filename) {
+        byte[] fileContentAsByteArray = readFile(filename);
+    }
 
+    private byte[] readFile(String filename) {
+        try {
+            return Files.readAllBytes(Paths.get(filename));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
